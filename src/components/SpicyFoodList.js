f9cf6fd75502997abccc9033bf09905d7bac5f05
Daryl Rogony <darylrogony@gmail.com>
2022-05-09 15:54:48 +0300

    Completed Assignment on manipulating arrray's state

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,22 +3,49 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
 
-  function handleAddFood() {
-    const newFood = getNewSpicyFood();
-    console.log(newFood);
-  }
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine == filterBy;
+    }
+  });
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+    function handleAddFood() {
+    const newFood = getNewSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+  }
+   console.log(handleAddFood);
+
+  function handleClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  }
+  
+  function handleFilterByChange(event) {
+   setFilterBy(event.target.value);
+ }
+
   return (
     <div>
-      <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
+      <select name="filter" onChange={handleFilterByChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
+      {/* <button onClick={handleAddFood}>Add New Food</button>
+      <ul>{foodList}</ul> */}
     </div>
   );
 }
